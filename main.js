ffba683962d88b19509554b22325f4efddd81d22
09092007geise <105931122+09092007geise@users.noreply.github.com>
2023-11-09 08:12:18 -0300

    ,..

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("Opa como vcs estão rapaziada!!")
+function tocaSom(idElementoAudio){
+document.querySelectorAll(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom(idAudio);
+
+    }
+    contador = contador+ 1;
+    console.log(contador);
+    
+}
